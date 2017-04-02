0711d75f05f63bc22927fe015e23098ebc80cdbd
Mugenx <sahawu.mail@gmail.com>
2017-04-01 22:53:58 -0400

    minor update

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,17 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-// before using ejs
-// router.get('/', function (req, res) {
-//     var content = '';
-//     content += '<link href="css/style.css" type="text/css" rel="stylesheet">';
-//     content += '<h1>HELLO WORLD</h1>';
-//     content += '<h1>THIS TUTORIAL IS FUCKING SUCK!!!</h1>';
-//     content += '<img src="/images/misc/background.jpg" alt="background" style="height: 300px">';
-//     content += '<script  src="/reload/reload.js"></script>';
-//     res.send(content);
-// });
-
 
 router.get('/', function (req, res) {
     var data = req.app.get('appData');
